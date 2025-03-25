43a77c2aa1186b38551a8016bf52c430341bd7ab
NATTHAWAT <natthawat.derb@gmail.com>
2025-03-25 09:59:14 +0700

    Create main.js

---
+++ sessions/chapter-3/main.js
@@ -0,0 +1,33 @@
+
+// 1. Strings = text //
+let firstname = 'Dave'
+const idcard = '1234'
+
+console.log('My name is',firstname)
+
+// 2.  Numbers //
+
+let age = 28
+let height = 1.70
+
+//3.Boolean 
+let isThai = false
+
+firstname = 'test'
+
+console.log('My name is',firstname , 'and my age is', age)
+
+//3.JavaScript Operators
+
+
+//The Assignment Operator = assigns values
+let d = 10;
+
+// Assign the value 5 to x
+let Numbers1 = 5;
+// Assign the value 2 to y
+let Numbers2  = 2;
+// Assign the value x + y to z:
+let Numbers4 = Numbers1 + Numbers2 ;
+
+let Numbers3 = Numbers1  * Numbers2 ;
