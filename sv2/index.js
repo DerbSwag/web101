1641bf40d6b1586fcc9693a8fed1441887794658
NATTHAWAT <natthawat.derb@gmail.com>
2025-03-28 10:57:49 +0700

    Update index.js

--- sv2/index.js
+++ sv2/index.js
@@ -1,16 +1,20 @@
 const express = require('express')
 const app = express()
 
-// สร้าง GET API path /user
-app.get('/user', (req, res) => {
-  res.json({
+// สร้าง GET API path /test
+app.get('/test', (req, res) => {
+  let user = {
     firstname: 'ชื่อจริง',
     lastname: 'นามสกุล',
     age: 20
-  });
+  }
+  res.json(user)
 })
+
+
+
 
 const port = 8000
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`)
+  console.log(`Server is running on port  ${port}`)
 })
