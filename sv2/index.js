430424bd795b86d8f6b2d67fccf2ecd6c0fab967
NATTHAWAT <natthawat.derb@gmail.com>
2025-03-29 11:20:44 +0700

    Update index.js

--- sv2/index.js
+++ sv2/index.js
@@ -1,16 +1,30 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 
-// สร้าง GET API path /user
-app.get('/user', (req, res) => {
-  res.json({
-    firstname: 'ชื่อจริง',
-    lastname: 'นามสกุล',
-    age: 20
-  });
+// Parse incoming JSON data
+app.use(bodyParser.json())
+
+// เราสร้างตัวแปร users ขึ้นมาเป็น Array จำลองการเก็บข้อมูลใน Server (ซึ่งของจริงจะเป็น database)
+let users = []
+
+// Route handler for creating a new user
+app.post('/user', (req, res) => {
+  const data = req.body
+
+  const newUser = {
+    firstname: data.firstname,
+    lastname: data.lastname,
+    age: data.age
+  }
+
+  //
+  users.push(newUser)
+
+  // Server ตอบกลับมาว่าเพิ่มแล้วเรียบร้อย
+  res.status(201).json({ message: 'User created successfully', user: newUser })
 })
 
-const port = 8000
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`)
+app.listen(8000, () => {
+  console.log('Server started on port 8000');
 })
